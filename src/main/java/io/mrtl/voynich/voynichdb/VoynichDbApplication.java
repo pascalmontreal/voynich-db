99ff51efa0ab3b76d2927341196b365a02003a17
Pascal Martel <pascal.martel@quebecormedia.com>
2019-10-04 23:04:50 -0400

    New Java structure

---
+++ src/main/java/io/mrtl/voynich/voynichdb/VoynichDbApplication.java
@@ -0,0 +1,13 @@
+package io.mrtl.voynich.voynichdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoynichDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoynichDbApplication.class, args);
+	}
+
+}
